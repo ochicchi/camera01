a15515277b5b65d4f7d1667625007447b9597c4c
Satoru Ohguchi <s.ohguchi@gmail.com>
2019-07-10 20:04:53 +0900

    Initial Commit

---
+++ camera01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  camera01
+//
+//  Created by Satoru Ohguchi on 2019/07/10.
+//  Copyright © 2019年 Satoru Ohguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
